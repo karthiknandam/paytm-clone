fe10d11da790fc1a23c0bd4b2aa933dccc28a658
Karthik Nandam <carthicknandam@gmail.com>
2025-02-14 18:29:25 +0530

    add test case -1

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,5 +3,5 @@
 import { useBalance } from "@repo/store/balance";
 export default function () {
   const balance = useBalance();
-  return <div>hi there {balance}</div>;
+  return <div>hi there from merge-test-v1 {balance}</div>;
 }
